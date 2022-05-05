66c4ed3f499102d8a556b40834868cee1b3936c6
YMRTZ <a76yang@uwaterloo.ca>
2022-05-05 13:08:19 -0400

    Removed composition bit, replaced with options

--- src/pages/Upload.vue
+++ src/pages/Upload.vue
@@ -7,8 +7,15 @@
 </template>
 
 <script lang="ts">
+import {
+  doc,
+  DocumentReference,
+  Firestore,
+  getDoc,
+  getFirestore,
+  setDoc,
+} from 'firebase/firestore';
 import { FirebaseApp, initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore/lite';
 import { getStorage, uploadBytes } from 'firebase/storage';
 import { ref as fireRef } from 'firebase/storage';
 import { defineComponent, Ref, ref } from 'vue';
@@ -17,36 +24,96 @@ const app: FirebaseApp = initializeApp(firebaseConfig);
 
 export default defineComponent({
   name: 'uploadComponent',
-  setup() {
-    const fileRef: Ref = ref(null);
-  },
+  // setup() {
+  //   const fileRef: Ref = ref(null);
+  // },
   data() {
     return {
-      firestore_database: getFirestore(),
+      fileRef: ref(null),
+      firestore_database: getFirestore(app),
       cloudstore_database: getStorage(),
     };
   },
   methods: {
-    uploadImage() {
+    async uploadImage() {
       console.log('Upload start');
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       let htmlRef: HTMLInputElement = this.$refs.fileRef as HTMLInputElement;
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       let fileRef = htmlRef.files;
       let testRef = fireRef(this.cloudstore_database, 'test.png');
-      console.log(this.cloudstore_database);
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+
+      let fileNameExtension: string;
+      let fileName = 'default';
+      let fileExtension: string;
+
       if (fileRef) {
-        console.log(fileRef[0]);
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        void uploadBytes(testRef, fileRef[0]).then((snapshot) => {
-          console.log('Uploaded');
-        });
+        fileNameExtension = fileRef[0].name;
+        fileName = fileNameExtension.slice(
+          0,
+          fileNameExtension.lastIndexOf('.')
+        );
+        fileExtension = fileRef[0].name.slice(
+          fileNameExtension.lastIndexOf('.')
+        );
+        console.log(fileNameExtension);
+        console.log(fileName);
+        console.log(fileExtension);
       }
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+      const firebaseFileRef = doc(this.firestore_database, 'Tags', fileName);
+      //Check if already added, add if not
+      await getDoc(firebaseFileRef).then((snapshot) => {
+        if (!snapshot.exists()) {
+          console.log('DNE');
+          const addFile = this.addToFirebase(firebaseFileRef, 0).then(() => {
+            console.log('Added');
+          });
+        }
+      });
+      //Update count
+      await getDoc(firebaseFileRef).then((snapshot) => {
+        if (snapshot.exists()) {
+          let count: number = snapshot.data().count as number;
+          count++;
+          const addFile = this.addToFirebase(firebaseFileRef, count).then(
+            () => {
+              console.log('Updated');
+              let storageRef = fireRef(
+                this.cloudstore_database,
+                fileName + '/' + count.toString()
+              );
+              console.log(storageRef);
+              if (fileRef) {
+                const uploadImage = uploadBytes(storageRef, fileRef[0]).then(
+                  () => {
+                    console.log('Uploaded');
+                  }
+                );
+              }
+            }
+          );
+        } else {
+          console.log('Something has gone very wrong!');
+        }
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      // if (fileRef) {
+      //   console.log(fileRef[0]);
+      //   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      //   void uploadBytes(testRef, fileRef[0]).then((snapshot) => {
+      //     console.log('Uploaded');
+      //   });
+      // }
+    },
+    async addToFirebase(
+      firebaseFileRef: DocumentReference,
+      updatedCount: number
+    ) {
+      await setDoc(firebaseFileRef, { count: updatedCount });
     },
   },
 });
 </script>
-
-function storageRef(storageRef: any, file: any) { throw new Error('Function not
-implemented.'); }
